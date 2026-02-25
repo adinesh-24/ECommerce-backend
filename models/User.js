5f084e3465eaa945171e4900ee67a698836d6b23
adinesh-24 <adinesh2470@gmail.com>
2026-02-25 20:12:58 +0530

    fix: bcrypt error

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -19,11 +19,24 @@ const userSchema = new mongoose.Schema({
 
     role: {
         type: String,
-        enum: ['user', 'manager', 'admin']
+        enum: ['user', 'manager', 'admin'],
+        default: 'user'
     }
 },
     {
         timestamps: true
     });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);

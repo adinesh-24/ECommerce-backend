43c73f5d864d6f0d9544a697d5eaf4b58ac04854
adinesh-24 <adinesh2470@gmail.com>
2026-02-26 17:05:27 +0530

    fix: change SMTP port to 465 for Render compatibility

--- config/mail.config.js
+++ config/mail.config.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // Use STARTTLS
+  port: 465,
+  secure: true, // Use SSL/TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
